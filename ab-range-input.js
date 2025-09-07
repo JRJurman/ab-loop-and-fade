0c7cc2824aa2743e3a46d8cb0bbc3790e1931457
Jesse Jurman <j.r.jurman@gmail.com>
2025-09-07 10:49:56 -0400

    new ab-range-input component

---
+++ ab-range-input.js
@@ -0,0 +1,127 @@
+const abRangeInputStyleSheet = new CSSStyleSheet();
+abRangeInputStyleSheet.replaceSync(`
+	label {
+		display: flex;
+		align-items: center;
+		gap: 1em;
+	}
+	input {
+		flex: 1;
+	}
+	output {
+		min-width: 2em;
+		text-align: right;
+	}
+	:host([format="ms"]) output {
+		min-width: 4em;
+	}
+`);
+
+class ABRangeInput extends HTMLElement {
+	static observedAttributes = ['value', 'disabled'];
+
+	constructor() {
+		super();
+
+		this.attachShadow({ mode: 'open' });
+
+		this.shadowRoot.adoptedStyleSheets = [abRangeInputStyleSheet];
+	}
+
+	get value() {
+		return this.getAttribute('value');
+	}
+	set value(newValue) {
+		if (this.isConnected) {
+			this.inputElement.value = newValue;
+		}
+		this.setAttribute('value', newValue);
+	}
+
+	get disabled() {
+		return this.getAttribute('disabled');
+	}
+	set disabled(newValue) {
+		this.setAttribute('disabled', newValue);
+		if (this.isConnected) {
+			this.inputElement.disabled = newValue;
+		}
+	}
+
+	get max() {
+		return this.getAttribute('max');
+	}
+	set max(newValue) {
+		if (this.isConnected) {
+			this.inputElement.max = newValue;
+		}
+		this.setAttribute('max', newValue);
+	}
+
+	connectedCallback() {
+		// populate the shadow root with all the elements this component needs
+		this.labelElement = document.createElement('label');
+		this.inputElement = document.createElement('input');
+		this.outputElement = document.createElement('output');
+		this.slotElement = document.createElement('slot');
+
+		this.inputElement.setAttribute('type', 'range');
+		this.inputElement.setAttribute('min', this.getAttribute('min'));
+		this.inputElement.setAttribute('max', this.getAttribute('max'));
+		this.inputElement.setAttribute('step', this.getAttribute('step'));
+		this.inputElement.setAttribute('value', this.getAttribute('value'));
+		if (this.hasAttribute('disabled')) {
+			this.inputElement.setAttribute('disabled', '');
+		}
+
+		this.labelElement.append(this.slotElement, this.inputElement, this.outputElement);
+		this.shadowRoot.append(this.labelElement);
+
+		// wire up event listener for input
+		this.inputElement.addEventListener('input', () => {
+			this.setAttribute('value', this.inputElement.value);
+			this.value = this.inputElement.value;
+			this.dispatchEvent(new Event('input', { bubbles: true, composed: true }));
+		});
+
+		this.updateOutputValue();
+	}
+
+	attributeChangedCallback(attributeName) {
+		// if we haven't created the shadowRoot, don't worry about updating anything
+		if (!this.isConnected) {
+			return;
+		}
+
+		// update the output element based on new a new value
+		if (attributeName === 'value') {
+			this.updateOutputValue();
+		}
+
+		if (attributeName === 'disabled') {
+			if (this.hasAttribute('disabled')) {
+				this.inputElement.setAttribute('disabled', '');
+			} else {
+				this.inputElement.removeAttribute('disabled');
+			}
+		}
+	}
+
+	updateOutputValue() {
+		const format = this.getAttribute('format');
+		if (format === 'time') {
+			const rawSeconds = this.inputElement.value % 60;
+			const seconds = rawSeconds < 10 ? `0${rawSeconds}` : rawSeconds;
+			const minutes = Math.floor(this.inputElement.value / 60);
+			this.outputElement.value = `${minutes}:${seconds}`;
+		} else if (format === 'float') {
+			this.outputElement.value = parseFloat(this.inputElement.value).toFixed(2);
+		} else if (format === 'ms') {
+			this.outputElement.value = `${this.inputElement.value} ms`;
+		} else {
+			this.outputElement.value = this.inputElement.value;
+		}
+	}
+}
+
+customElements.define('ab-range-input', ABRangeInput);
